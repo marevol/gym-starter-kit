e3d732d0a4371cd44c91cc803d2d37fcd864ab3e
Shinsuke Sugaya <shinsuke@yahoo.co.jp>
2016-12-26 16:56:42 +0900

    version 0.0.3.dev1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="gym-starter-kit",
-    version="0.0.2",
+    version="0.0.3.dev1",
     packages=['gymkit',
               'gymkit.agent'],
     author="Shinsuke Sugaya",
@@ -13,7 +13,7 @@ setup(
     description=("Gym Starter Kit: A running environment for developing OpenAI Gym agents."),
     keywords="machine learning",
     url="https://github.com/marevol/gym-starter-kit",
-    download_url='https://github.com/marevol/gym-starter-kit/tarball/0.0.2',
+    download_url='https://github.com/marevol/gym-starter-kit/tarball/0.0.3',
     classifiers=[
         "Programming Language :: Python :: 3",
         "Development Status :: 2 - Pre-Alpha",
